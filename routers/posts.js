9c4c26103d4ec2c15cf5148ab128912aa5e9530d
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-27 16:16:45 +0100

    feat: add routes

--- routers/posts.js
+++ routers/posts.js
@@ -1,3 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.send('Lista dei post');
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params
+    res.send('Dettagli dei post ' + id);
+})
+
+router.post('/', (req, res) => {
+    res.send('Creazione nuovo post');
+})
+
+router.put('/:id', (req, res) => {
+    const {id} = req.params;
+    res.send('Modifica integrale dei post ' + id);
+})
+
+router.patch('/:id', (req, res) => {
+    const {id} = req.params;
+    res.send('Modifica parziale dei post ' + id);
+})
+
+router.delete('/:id', (req, res) => {
+    const {id} = req.params;
+    res.send('Eliminazione del post ' + id);
+})
+
