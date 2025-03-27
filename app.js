08b18db0f24ca331233b5fda3b491ba0628f99f3
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-27 16:41:22 +0100

    feat: add router's link

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+// express
 const express = require('express');
 const app = express();
-const port = 3000;
 
+// porta
+const port = 3000;
 app.listen(port,() => {
     console.log('sono un server attivo sulla porta ' + port);
 })
+
+// router
+const postsRouter = require('./routers/posts');
+app.use('/posts', postsRouter);
 
--- routers/posts.js
+++ routers/posts.js
@@ -1,31 +1,40 @@
+// express
 const express = require('express');
 const router = express.Router();
 
+// index
 router.get('/', (req, res) => {
     res.send('Lista dei post');
 });
 
+// show
 router.get('/:id', (req, res) => {
     const {id} = req.params
     res.send('Dettagli dei post ' + id);
 })
 
+// store
 router.post('/', (req, res) => {
     res.send('Creazione nuovo post');
 })
 
+// update
 router.put('/:id', (req, res) => {
     const {id} = req.params;
     res.send('Modifica integrale dei post ' + id);
 })
 
+// modify
 router.patch('/:id', (req, res) => {
     const {id} = req.params;
     res.send('Modifica parziale dei post ' + id);
 })
 
+// destroy
 router.delete('/:id', (req, res) => {
     const {id} = req.params;
     res.send('Eliminazione del post ' + id);
 })
 
+// esportare router
+module.exports = router;
